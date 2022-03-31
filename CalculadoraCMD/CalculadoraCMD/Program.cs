0ae0da805061d453a62c9596297fc539a085068d
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-31 07:34:46 -0300

    primeiros passos

--- CalculadoraCMD/CalculadoraCMD/Program.cs
+++ CalculadoraCMD/CalculadoraCMD/Program.cs
@@ -7,12 +7,32 @@ namespace CalculadoraCMD
 {
     internal class Program
     {
+
+        enum Menu
+        {
+            Soma = 1,
+            Subtracao = 2,
+            Multiplicacao = 3,
+            Potencia = 4,
+            Raiz = 5,
+            Sair = 6,
+        } 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreva Seu Nome: ");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Seu nome é : " + nome);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
 
+            Console.WriteLine("_________________________________________________________________\n");
+            Console.WriteLine("Bem Vindo a Minha Primeira Calculadora!\nSelicione uma das opções:");
+            Console.WriteLine("_________________________________________________________________\n");
+
+            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Potencia\n5 - Raiz\n6 - Sair");
+
+                        /*Casting*/
+            Menu opcao = (Menu)int.Parse(Console.ReadLine());
+
+            Console.WriteLine(opcao);
 
             Console.ReadLine();
         
