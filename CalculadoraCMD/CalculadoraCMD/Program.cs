83e572464f7ddc34a5314e0d88c0687f6c00c41c
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-04-01 07:40:50 -0300

    Operações Básicas enviada

--- CalculadoraCMD/CalculadoraCMD/Program.cs
+++ CalculadoraCMD/CalculadoraCMD/Program.cs
@@ -13,9 +13,10 @@ namespace CalculadoraCMD
             Soma = 1,
             Subtracao = 2,
             Multiplicacao = 3,
-            Potencia = 4,
-            Raiz = 5,
-            Sair = 6,
+            Divisão = 4,
+            Potencia = 5,
+            Raiz = 6,
+            Sair = 7,
         } 
 
         static void Main(string[] args)
@@ -31,25 +32,119 @@ namespace CalculadoraCMD
             Console.WriteLine("Bem Vindo a Minha Primeira Calculadora!\nSelicione uma das opções:");
             Console.WriteLine("_________________________________________________________________\n");
 
-            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Potencia\n5 - Raiz\n6 - Sair");
+            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Dvisão\n5 - Potencia\n6 - Raiz\n7 - Sair");
 
                         /*Casting*/
             Menu opcao = (Menu)int.Parse(Console.ReadLine());
 
             Console.WriteLine(opcao);
-
+            
             switch (opcao)
-	        {
-	            case Menu.Sair:
-                        escolheuSair = true;
-                break;
-	        }
+	            {
+		            case Menu.Soma:
+                        Soma();
+                        break;
 
+                    case Menu.Subtracao:
+                        Subtracao();
+                        break;
+
+                     case Menu.Multiplicacao:
+                        Multiplicacao();
+                     break;
+
+                        case Menu.Divisão:
+                        Divisao();
+                        break;
+                        
+                    /*case Menu.Potencia:
+                        Potencia();
+                        break;
+                    */
+                    case Menu.Raiz:
+                     break;
+                    case Menu.Sair:
+                                escolheuSair = true;
+                     break;
+                  
+	             }
+
+          
+                Console.Clear();
             }
 
 
             
         
         }
+
+        static void Soma ()
+        {
+            Console.WriteLine("Soma de dois Numeros:");
+            Console.WriteLine("Digite o primeiro Numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro Segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int resultado  = a + b;
+            Console.WriteLine($"O resultado é: {resultado}");
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+        }
+
+        static void Subtracao()
+        {
+            Console.WriteLine("Subtração de dois Numeros:");
+            Console.WriteLine("Digite o primeiro Numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int resultado  = a - b;
+            Console.WriteLine($"O resultado da subtrção entre {a} e {b}: {resultado}");
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.WriteLine("Multiplicação de dois Numeros:");
+            Console.WriteLine("Digite o primeiro Numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int resultado  = a * b;
+            Console.WriteLine($"O resultado da multiplicação entre {a} e {b}: {resultado}");
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+        }
+
+        static void Divisao()
+        {
+            Console.WriteLine("Divisão de dois Numeros:");
+            Console.WriteLine("Digite o primeiro Numero: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            int b = int.Parse(Console.ReadLine());
+            int resultado  = a / b;
+            Console.WriteLine($"O resultado da divisão entre {a} e {b}: {resultado}");
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+        }
+
+        /*
+
+        static void double Potencia()
+        {
+            Console.WriteLine("Potência de dois Numeros:");
+            Console.WriteLine("Digite o primeiro Numero: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            double b = double.Parse(Console.ReadLine());
+            double resultado = Math.Pow(a,b);
+            Console.WriteLine($"O resultado da potência entre {a} e {b}: {resultado}");
+            Console.WriteLine("Aperte ENTER para voltar para o menu");
+            Console.ReadLine();
+        }
+        */
     }
+
 }
